88180c4d69bad318c8fe12e497f6599283ecf2d8
minedcaesar <fanara.gaspare@gmail.com>
2026-05-06 15:55:40 +0000

    read_catalog compiles and works

--- library.c
+++ library.c
@@ -1,15 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
 typedef struct {
     char name[100];
     char author[100];
     int year;
 } Book;
+int count_lines(char file_name[]){
+    char c;
+    FILE *fp = fopen(file_name,"r");
+    int count =0;
+    for (c = getc(fp); c != EOF; c = getc(fp)) {
+        if (c == '\n') {
+            count++;
+        }
+    }
+    return count;
+}
 
+
+Book* read_catalog(char *catalog_file,int lines){
+    int MAX_FIELD_LENGTH=1024;
+    FILE *fp = fopen(catalog_file,"r");
+    if (!fp) return NULL;
+    Book* catalog = (Book*)malloc(lines*sizeof(Book));
+    char row[MAX_FIELD_LENGTH];
+    int current_book=0;
+    while (fgets(row, sizeof(row), fp) && current_book < lines) {
+        
+        row[strcspn(row, "\r\n")] = 0;
+        
+        if (strlen(row) == 0) continue;
+
+        char *token = strtok(row, ",");
+        if (token != NULL) {
+            strncpy(catalog[current_book].name, token, sizeof(catalog[current_book].name) - 1);
+            catalog[current_book].name[sizeof(catalog[current_book].name) - 1] = '\0';
+        }
+        token = strtok(NULL, ",");
+        if (token != NULL) {
+            strncpy(catalog[current_book].author, token, sizeof(catalog[current_book].name) - 1);
+            catalog[current_book].author[sizeof(catalog[current_book].name) - 1] = '\0';
+        }
+        token = strtok(NULL, ",");
+        if (token != NULL) {
+            catalog[current_book].year=atoi(token);
+        }
+        current_book++;
+    }
+    fclose(fp);
+    return catalog;
+}
 
 int main(int argc, char* argv[]) {
-
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <library_id> <num_libraries> <catalog_file>\n", argv[0]);
+        return 1;
+    }
+    int id = atoi(argv[1]);
+    char *catalog_file = argv[3];
+    int lines = count_lines(catalog_file);
+    Book * catalog = read_catalog(catalog_file,lines);
+    for (int i=0;i<lines;i++){
+        printf("Book %d: Name: %s, Author %s, Year %d\n",i+1,catalog[i].name,catalog[i].author,catalog[i].year);
+    }
 }
 
