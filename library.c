50ac565ef7e57d978f8be5495ffd8d4f3c62b787
minedcaesar <fanara.gaspare@gmail.com>
2026-05-06 14:37:28 +0000

    Yes

--- library.c
+++ library.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef struct {
+    char name[100];
+    char author[100];
+    int year;
+} Book;
+
+
+int main(int argc, char* argv[]) {
+
+}
 
