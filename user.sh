b5b89e5d13cc7a2724681bf637ed284eb3d8bbe9
Chaitanya-jaiswal <chaitanya.jaiswal@studenti.unitn.it>
2026-05-15 21:39:30 +0200

    Complete user.sh — all four operations implemented and syntax verified

--- user.sh
+++ user.sh
@@ -36,10 +36,43 @@ function check_library_pipe_status() {
 function check_num_args_for_borrow_return() {
     local num_args=$1
     if [ $num_args -lt 4 ]; then
-            echo "Invalid arguement for borrowing/ returning book"
-            echo "USAGE: ./user.sh <username> <library_id> <BORROW / RETURN> <book_title>"
-            exit 1
+        echo "Invalid arguement for borrowing/ returning book"
+        echo "USAGE: ./user.sh <username> <library_id> <BORROW / RETURN> <book_title>"
+        exit 1
     fi
+}
+
+function check_num_args_for_search() {
+    local num_args=$1
+    if [ $num_args -lt 6 ]; then
+        echo "Invalid arguement for searching a book"
+        echo "USAGE: ./user.sh <username> <library_id> <SEARCH> <--by author/ title/ year> <AUTHOR/ TITLE/ YEAR>"
+        exit 1
+    fi
+}
+
+function check_flag_for_search() {
+    local search_by=$1
+    if [ "$search_by" != "--by" ]; then
+        echo "ERROR: Expected --by flag"
+        exit 1
+    fi
+}
+
+function handle_search_request() {
+    local field=$1
+    local value=$2
+
+    case $field in
+        author|title|year)
+            request_message="SEARCH|${username}|${field}|${value}|${RESPONSE_PIPE}"
+            echo "$request_message"
+        ;;
+        *)
+            echo "ERROR: Invalid search field '$field'. Use: author, title, year"
+            exit 1
+        ;;
+    esac
 }
 
 function process_request() {
@@ -78,9 +111,17 @@ case $operation in
         process_request "$request_message"
         process_response
     ;;
-    # SEARCH)
-    # #TODO
-    # ;;
+    SEARCH)
+        #SEARCH OPERATION
+        check_num_args_for_search $#
+        search_by=$4
+        search_field=$5
+        operation_arguement=$6
+        check_flag_for_search "$search_by"
+        request_message=$(handle_search_request "$search_field" "$operation_arguement")
+        process_request "$request_message"
+        process_response
+    ;;
     BORROW)
         #BORROW OPERATION
         check_num_args_for_borrow_return $#
