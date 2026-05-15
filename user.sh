7dfb16301baf6a72b625617c45124cf7a4efa96c
Chaitanya-jaiswal <chaitanya.jaiswal@studenti.unitn.it>
2026-05-15 20:18:37 +0200

    Implemented REGISTER, BORROW, RETURN operations with pipe IPC, response parsing, and input validation

--- user.sh
+++ user.sh
@@ -10,11 +10,11 @@ if [ $# -lt 3 ]; then
     exit 1
 fi
 
-username=$1
+username=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 library_id=$2
 operation=$(echo "$3" | tr '[:lower:]' '[:upper:]')
 
-RESPONSE_PIPE=/tmp/user_response_$$
+RESPONSE_PIPE=/tmp/"$username"_$$
 mkfifo $RESPONSE_PIPE
 
 if [ $? -ne 0 ]; then
@@ -24,24 +24,78 @@ fi
 
 trap "rm -f $RESPONSE_PIPE" EXIT
 
-REQUEST_PIPE=/tmp/library_"$library_id"_request
+REQUEST_PIPE=/tmp/library_"$library_id"
+
+function check_library_pipe_status() {
+    if [ ! -p "$REQUEST_PIPE" ]; then
+        echo "ERROR: Library $library_id is not running"
+        exit 1
+    fi
+}
+
+function check_num_args_for_borrow_return() {
+    local num_args=$1
+    if [ $num_args -lt 4 ]; then
+            echo "Invalid arguement for borrowing/ returning book"
+            echo "USAGE: ./user.sh <username> <library_id> <BORROW / RETURN> <book_title>"
+            exit 1
+    fi
+}
+
+function process_request() {
+    local request=$1
+    check_library_pipe_status
+    echo "$request" > $REQUEST_PIPE &
+}
+
+function process_response() {
+    response=$(timeout 10 head -1 $RESPONSE_PIPE)
+
+    if [ -z "$response" ]; then
+        echo "ERROR: No response received from the library: $library_id"
+        exit 1
+    fi
+
+    status_code=$(echo "$response" | cut -d'|' -f1)
+    message=$(echo "$response" | cut -d'|' -f2-)
+
+    case $status_code in
+        0)
+        echo "$message"
+        exit 0
+        ;;
+        *)
+        echo "$message"
+        exit 1
+        ;;
+    esac
+}
 
 case $operation in
     REGISTER)
-    #REGISTER OPERATION
-
+        #REGISTER OPERATION
+        request_message="REGISTER|${username}|${RESPONSE_PIPE}"
+        process_request "$request_message"
+        process_response
     ;;
-    SEARCH)
-    #SEARCH OPERATION
-
-    ;;
+    # SEARCH)
+    # #TODO
+    # ;;
     BORROW)
-    #BORROW OPERATION
-
+        #BORROW OPERATION
+        check_num_args_for_borrow_return $#
+        book_title=$4
+        request_message="BORROW|${username}|${book_title}|${RESPONSE_PIPE}"
+        process_request "$request_message"
+        process_response
     ;;
     RETURN)
-    #RETURN OPERATION
-
+        #RETURN OPERATION
+        check_num_args_for_borrow_return $#
+        book_title=$4
+        request_message="RETURN|${username}|${book_title}|${RESPONSE_PIPE}"
+        process_request "$request_message"
+        process_response
     ;;
     *)
     #default
